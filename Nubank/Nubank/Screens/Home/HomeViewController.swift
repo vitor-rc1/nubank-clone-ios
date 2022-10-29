e809758e435f7f94ab9f05a15089a30152fd69ce
vitor-rc1 <vitor_rc@outlook.com>
2022-10-29 12:05:40 -0300

    Added SwiftGen to the project

--- Nubank/Nubank/Screens/Home/HomeViewController.swift
+++ Nubank/Nubank/Screens/Home/HomeViewController.swift
@@ -12,7 +12,6 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("View controller")
     }
 
 }
